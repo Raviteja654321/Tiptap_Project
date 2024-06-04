ceebe1f9399709ce9f3edef2a902226bb541046b
Raviteja654321 <raviteja.nagulapalli@students.iiit.ac.in>
2024-06-04 20:14:58 +0530

    Added inline css

--- src/CustomTable.js
+++ src/CustomTable.js
@@ -1,135 +1,79 @@
-import { mergeAttributes } from "@tiptap/react";
-import  Table from "@tiptap/extension-table";
+import Table from "@tiptap/extension-table";
 import TableRow from '@tiptap/extension-table-row'
 import TableHeader from '@tiptap/extension-table-header'
 import TableCell from '@tiptap/extension-table-cell'
-import Paragraph from '@tiptap/extension-paragraph'
 
- const CustomTable = Table.extend({
-  addAttributes() {
-    return {
-      style: {
-        default: null,
-        parseHTML: (element) => {
-          return element.hasAttribute("style")
-            ? element.getAttribute("style")
-            : null;
-        },
-      },
-    };
-  },
-
-  addOptions() {
-    return {
-      HTMLAttributes: {
-        style: "color: 'pink'; border-collapse: collapse; margin: 0; overflow: hidden; table-layout: fixed; width: 100%;",
-      },
-    };
-  },
-
-  renderHTML({ HTMLAttributes }) {
-    return [
-      "td",
-      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
-      ["tbody", 0],
-    ];
-  },
-});
-
- const CustomTableRow = TableRow.extend({
-  addAttributes() {
-    return {
-      style: {
-        default: null,
-        parseHTML: (element) => {
-          return element.hasAttribute("style")
-            ? element.getAttribute("style")
-            : null;
-        },
-      },
-    };
-  },
-
-//   addOptions() {
-//     return {
-//       HTMLAttributes: {
-//         style: "",
-//       },
-//     };
-//   },
-
-  renderHTML({ HTMLAttributes }) {
-    return ["tr", mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
-  },
-});
-
- const CustomTableCell = TableCell.extend({
-  addAttributes() {
-    return {
-      style: {
-        default: null,
-        parseHTML: (element) => {
-          return element.hasAttribute("style")
-            ? element.getAttribute("style")
-            : null;
-        },
-      },
-    };
-  },
-
-  addOptions() {
-    return {
-      HTMLAttributes: {
-        style: "border: 2px solid #ced4da; box-sizing: border-box; min-width: 1em; padding: 3px 5px; position: relative; vertical-align: top;",
-      },
-    };
-  },
-
-  renderHTML({ HTMLAttributes }) {
-    return ["td", mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
-  },
-});
-
- const CustomTableHeader = TableHeader.extend({
-  addOptions() {
-    return {
-      HTMLAttributes: {
-        style: "background-color: #f1f3f5; font-weight: bold; text-align: left; border: 2px solid #ced4da; box-sizing: border-box; min-width: 1em; padding: 3px 5px; position: relative; vertical-align: top;",
-      },
-    };
-  },
-
-  content: 'inline*',
-
-  tableRole: 'header_cell',
-
-  isolating: true,
-
-  parseHTML() {
-    return [{ tag: 'th' }]
-  },
-
-  renderHTML({ HTMLAttributes }) {
-    return ['th', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
-  },
-});
-
-const CustomParagraph = Paragraph.extend({
+// Custom Table Extension with Enhanced CSS
+const CustomTable = Table.extend({
     addAttributes() {
       return {
         color: {
-          default: 'pink',
-          renderHTML: attributes => ( { style: `color: ${attributes.color}` } ),
-          // Allow setting a custom color through a constructor argument
-        //   parse: value => (typeof value === 'string' ? value : 'pink'), // Handle invalid values gracefully
+          renderHTML: (attributes) => {
+            return {
+              // style: `border-collapse: collapse; margin: 0; overflow: hidden; table-layout: fixed; width: 100%; background-color: #f0f8ff;`,
+            };
+          },
         },
       };
     },
-
-    
-
   });
   
+  const CustomTableRow = TableRow.extend({
+    addAttributes() {
+      return {
+        color: {
+          renderHTML: (attributes) => {
+            return {
+              style: `height: 100%;`,
+            };
+          },
+        },
+      };
+    },
+  });
   
-
-export { CustomTable, CustomTableRow, CustomTableHeader, CustomTableCell, CustomParagraph };
+  const CustomTableCell = TableCell.extend({
+    addAttributes() {
+      return {
+        color: {
+          renderHTML: (attributes) => {
+            return {
+              style: `
+                border: 1px solid #ced4da;
+                min-width: 1em;
+                position: relative;
+                vertical-align: top;
+                height: 100%;
+              `,
+            };
+          },
+        },
+      };
+    },
+  });
+  
+  const CustomTableHeader = TableHeader.extend({
+    addAttributes() {
+      return {
+        color: {
+          renderHTML: (attributes) => {
+            return {
+              style: `
+                background-color: #262626;
+                color: #ffffff;
+                font-weight: bold;
+                text-align: center;
+                border: 1px solid #ced4da;
+                min-width: 1em;
+                position: relative;
+                vertical-align: top;
+                height: 100%;
+              `,
+            };
+          },
+        },
+      };
+    },
+  });
+  
+  export { CustomTable, CustomTableRow, CustomTableHeader, CustomTableCell };
--- src/Tiptap.jsx
+++ src/Tiptap.jsx
@@ -1,58 +1,62 @@
 import React from 'react';
-import {useEditor } from '@tiptap/react';
+import { useEditor, EditorContent } from '@tiptap/react';
 import Document from '@tiptap/extension-document';
 import Gapcursor from '@tiptap/extension-gapcursor';
-// import Paragraph from '@tiptap/extension-paragraph';
+import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
-import { CustomTable, CustomTableRow, CustomTableHeader, CustomTableCell ,CustomParagraph} from './CustomTable.js';
+import { CustomTable, CustomTableRow, CustomTableHeader, CustomTableCell} from './CustomTable.js';
 
 const tableWrapperStyles = {
-  padding: '1rem 0',
-  overflowX: 'auto',
+    padding: '1rem 0',
+    overflowX: 'auto',
 };
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [
-      Document,
-      // Paragraph,
-      Text,
-      Gapcursor,
-      CustomTable,
-      CustomTableRow,
-      CustomTableHeader,
-      CustomTableCell,
-      CustomParagraph,
-    ],
-    content: `
+    const editor = useEditor({
+        extensions: [
+            Document,
+            Paragraph,
+            Text,
+            Gapcursor,
+            CustomTable,
+            CustomTableRow,
+            CustomTableHeader,
+            CustomTableCell,
+        ],
+        content: `
+        <p>Hello World</p>
+        
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>singer</td>
+              <td>songwriter</td>
+              <td>actress</td>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>singer</td>
+              <td>songwriter</td>
+              <td>actress</td>
+            </tr>
+          </tbody>
+        </table>
     `,
-  });
+    });
 
-  if (!editor) {
-    return null;
-  }
-  console.log("Hello")
-  return (
-    <div style={tableWrapperStyles}>
-      <p>Hello all</p>
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-          <tr>
-            <td>Cyndi Lauper</td>
-            <td>singer, songwriter, actress</td>
-          </tr>
-          <tr>
-            <td>Bob Dylan</td>
-            <td>singer-songwriter, musician</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    if (!editor) {
+        return null;
+    }
+    return (
+        <div style={tableWrapperStyles}>
+            <EditorContent editor={editor} />
+        </div>
+    );
 };
 
 export default Tiptap;
