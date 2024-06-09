6638d2508a756bf1bc761b2ac7e3116596285e00
Raviteja654321 <raviteja.nagulapalli@students.iiit.ac.in>
2024-06-10 03:06:58 +0530

    Added Popover,Todo: cursor visibility and missing functionality of popover

--- src/CustomTable.js
+++ src/CustomTable.js
@@ -2,51 +2,77 @@ import TableHeader from '@tiptap/extension-table-header';
 import TableCell from '@tiptap/extension-table-cell';
 import Table from '@tiptap/extension-table';
 import Popover from './Popover';
+import {undo,redo, history } from '@tiptap/pm/history'
 import { ReactNodeViewRenderer } from '@tiptap/react';
+import { keymap } from '@tiptap/pm/keymap';
 
-// class NodeSelection extends Selection
 
 const CustomTable = Table.extend({
-    addAttributes() {
-        return {
-            ...this.parent?.(),
-            border_collapse: {
-                default: 'collapse',
-                renderHTML: (attributes) => {
-                    return {
-                        style: `
-                            margin: 2rem;
-                            border-collapse: ${attributes.border_collapse};
-                        `,
-                    };
-                },
-            }
-        };
-    },
     addOptions() {
         return {
-            resizable: true
+            resizable: true,
+            HTMLAttributes: {
+                style: `
+                    border-collapse: collapse;
+                `,
+            },
+            allowTableNodeSelection: true,
         }
     }
 })
 
 const CustomTableCell = TableCell.extend({
+    
+    addOptions() {
+        return {
+            HTMLAttributes: {
+                style: `
+                        position: relative;
+                        height: 20px;
+                        border: 2px solid #ced4da
+                    `,
+                class: 'table-cell',
+            }
+        }
+    },
+
     addAttributes() {
         return {
             width: {
-                default: '100px',
-                renderHTML: () => ({
+                default: '150px',
+                renderHTML: (attributes) => ({
                     style: `
-                        border: 1px solid #ced4da;
-                        position: relative;
-                        height: auto;
+                        width: ${attributes.width};
                     `,
-                    class: 'table-cell content',
                 }),
                 parseHTML: (element) => element.style.width.replace('px', ''),
             },
+            colwidth: {
+                default: null,
+                parseHTML: (element) => {
+                    const colwidth = element.getAttribute("colwidth");
+                    const value = colwidth ? [parseInt(colwidth, 10)] : null;
+
+                    return value;
+                },
+                renderHTML: attributes => {
+                    return {
+                        colwidth: attributes.colwidth,
+                        style: attributes.style ? `width: ${attributes.colwidth}px` : null
+                    }
+                }
+            },
         };
     },
+
+    addProseMirrorPlugins() {
+        return [
+          history(),
+          keymap({"Mod-z": undo, "Mod-y": redo})
+          
+          // …
+        ]
+      },
     
     addNodeView() {
         return ReactNodeViewRenderer(Popover,{as:'td'});
@@ -54,6 +80,21 @@ const CustomTableCell = TableCell.extend({
 });
 
 const CustomTableHeader = TableHeader.extend({
+    addOptions() {
+        return {
+            HTMLAttributes: {
+                style: `
+                    background-color: #2D323B;
+                    color: #ffffff;
+                    font-weight: bold;
+                    text-align: center;
+                    border: 1px solid #ced4da;
+                    position: relative;
+                    height:20px;
+                `
+            }
+        }
+    },
     addAttributes() {
         return {
             width: {
@@ -61,12 +102,6 @@ const CustomTableHeader = TableHeader.extend({
                 renderHTML: (attributes) => {
                     return {
                         style: `
-                            background-color: #2D323B;
-                            color: #ffffff;
-                            font-weight: bold;
-                            text-align: center;
-                            border: 1px solid #ced4da;
-                            position: relative;
                             width: ${attributes.width};
                         `
                     };
--- src/Popover.jsx
+++ src/Popover.jsx
@@ -1,11 +1,12 @@
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react';
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFill, faEraser, faTrashAlt, faArrowUp, faArrowDown, faArrowLeft, faArrowRight, faCaretSquareDown, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
+import { faFill, faEraser,faCircle, faTrashAlt, faArrowUp, faArrowDown, faArrowLeft, faArrowRight, faCaretSquareDown, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
 
 const Popover = ({ editor, selected }) => {
     const [showDropdown, setShowDropdown] = useState(false);
     const [isfocused, setIsfocused] = useState(false);
+    const [showColors, setShowColors] = useState(false);
 
     const handleBackgroundColorChange = (color) => {
         editor.chain().focus().setCellAttribute('backgroundColor', color).run();
@@ -39,24 +40,19 @@ const Popover = ({ editor, selected }) => {
         editor.chain().focus().addColumnAfter().run();
     };
 
-    // if (editor) {
-    //     console.log("The cell is selected.");
-    // }
-
     return (
         <NodeViewWrapper
             className="react-component-with-content"
-            onMouseEnter = {() => setIsfocused(true)}
-            onMouseLeave = {() => {setIsfocused(false); setShowDropdown(false)}}
+            onMouseEnter={() => setIsfocused(true)}
+            onMouseLeave={() => { setIsfocused(false); setShowDropdown(false) }}
             style={{
                 display: "flex",
                 width: '100%',
                 position: 'relative',
-                border: '1px solid #ced4da',
                 height: 'auto'
             }}>
             <NodeViewContent className="content" style={{ display: "flex", width: 'fit-content', margin: '0', padding: '0' }} />
-            {isfocused &&  (
+            {isfocused && (
                 <button
                     className="label"
                     onClick={() => setShowDropdown(!showDropdown)}
@@ -77,22 +73,69 @@ const Popover = ({ editor, selected }) => {
                 <div className="dropdown-container">
                     <ul className="popover-list">
                         <li>
-                            <button onClick={() => handleBackgroundColorChange('#ffffff')}>
-                                <FontAwesomeIcon icon={faFill} style={{ marginRight: '0.5rem' }} /> White
+                            <button
+                                onMouseEnter={() => setShowColors(true)}
+                            >
+                                <FontAwesomeIcon icon={faFill} style={{ marginRight: '0.5rem' }} />Background Color 
                             </button>
+                            {
+                                showColors &&
+                                <div className="dropdown-colors-container">
+                                    <ul className='popover-colors'>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#ffffff')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#ffffff', marginRight: '0.5rem' }} /> White
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#add8e6')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#add8e6', marginRight: '0.5rem' }} /> Light Blue
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#e0e0e0')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#e0e0e0', marginRight: '0.5rem' }} /> Gray
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#ffc0cb')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#ffc0cb', marginRight: '0.5rem' }} /> Pink
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#ffff00')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#ffff00', marginRight: '0.5rem' }} /> Yellow
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#0000ff')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#0000ff', marginRight: '0.5rem' }} /> Blue
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#000000')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#000000', marginRight: '0.5rem' }} /> Black
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#ffa500')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#ffa500', marginRight: '0.5rem' }} /> Orange
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button onClick={() => handleBackgroundColorChange('#ee82ee')}>
+                                                <FontAwesomeIcon icon={faCircle} style={{ color: '#ee82ee', marginRight: '0.5rem' }} /> Violet
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            }
                         </li>
                         <li>
-                            <button onClick={() => handleBackgroundColorChange('#f0f0f0')}>
-                                <FontAwesomeIcon icon={faFill} style={{ marginRight: '0.5rem' }} /> Light Gray
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={() => handleBackgroundColorChange('#e0e0e0')}>
-                                <FontAwesomeIcon icon={faFill} style={{ marginRight: '0.5rem' }} /> Gray
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={clearCell}>
+                            <button
+                                onClick={clearCell}
+                                onMouseEnter={() => setShowColors(false)}
+                            >
                                 <FontAwesomeIcon icon={faEraser} style={{ marginRight: '0.5rem' }} /> Clear Cell
                             </button>
                         </li>
