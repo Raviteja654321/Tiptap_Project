3c6a3069b0497cb4fa78e84e783fcd9e6336ef3e
Raviteja654321 <raviteja.nagulapalli@students.iiit.ac.in>
2024-06-14 14:28:41 +0530

    Parent table found using helper function

--- src/components/TableCellNodeView.jsx
+++ src/components/TableCellNodeView.jsx
@@ -1,40 +1,66 @@
-import {faCaretSquareDown}                                 from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon }                                 from '@fortawesome/react-fontawesome';
-import                                                          'tippy.js/dist/tippy.css';
-import DropdownContent                                     from './DropdownContent';
+import React, { useEffect, useState, useRef } from 'react';
+import tippy from 'tippy.js';
 import { NodeViewWrapper, NodeViewContent, ReactRenderer } from '@tiptap/react';
-import tippy                                               from 'tippy.js';
-import React, { useEffect, useState, useRef }              from 'react';
+import DropdownContent from './DropdownContent';
+import 'tippy.js/dist/tippy.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretSquareDown } from '@fortawesome/free-solid-svg-icons';
 
-const TableCellNodeView = ({editor, getPos, node }) => {
-    
-    const [isfocused, setIsfocused] = useState(false);
-    const [cell,setCell]            = useState();
-    const dropdownButtonRef         = useRef(null);
-    const { from, to }              = editor.state.selection;
-    const nodeFrom                  = getPos();
-    const nodeTo                    = nodeFrom + node.nodeSize;
+// Helper function to find the parent node
+const findParentClosestToPos = ($pos, predicate) => {
+    const depth = $pos.depth;
+
+    for (let i = depth; i > 0; i -= 1) {
+        const node = $pos.node(i);
+        if (predicate(node)) {
+            return { pos: $pos.before(i), node };
+        }
+    }
+
+    return undefined;
+};
+
+const TableCellNodeView = ({ editor, getPos, node }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const [cell, setCell] = useState(null);
+    const dropdownButtonRef = useRef(null);
+    const [parentTable, setParentTable] = useState(null);
+
+    const { from, to } = editor.state.selection;
+    const nodeFrom = getPos();
+    const nodeTo = nodeFrom + node.nodeSize;
 
     useEffect(() => {
-        setIsfocused(from >= nodeFrom && to <= nodeTo);
-    }, [from, to, nodeFrom, nodeTo]);
+        const isNodeFocused = from >= nodeFrom && to <= nodeTo;
+        setIsFocused(isNodeFocused);
+
+        if (isNodeFocused) {
+            const resolvedPos = editor.state.doc.resolve(nodeFrom);
+            const tableNode = findParentClosestToPos(resolvedPos, node => node.type.name === 'table');
+            if (tableNode) {
+                setParentTable(tableNode.node);
+                console.log("Table is => ",parentTable);
+            }
+        }
+    }, [editor, from, to, nodeFrom, nodeTo]);
 
     // Tippy instance is created
     useEffect(() => {
-        if (dropdownButtonRef.current && isfocused) {
-            const renderer = new ReactRenderer(DropdownContent, { 
-                props: { 
-                    editor, 
-                    getPos, 
-                    node, 
+        if (dropdownButtonRef.current && isFocused) {
+            const renderer = new ReactRenderer(DropdownContent, {
+                props: {
+                    editor,
+                    getPos,
+                    node,
                     cell,
+                    parentTable,
                     closeDropdown: () => {
                         if (tippyInstance) {
                             tippyInstance.hide();
                         }
                     }
-                }, 
-                editor 
+                },
+                editor
             });
 
             const tippyInstance = tippy(dropdownButtonRef.current, {
@@ -50,21 +76,23 @@ const TableCellNodeView = ({editor, getPos, node }) => {
                 tippyInstance.destroy();
             };
         }
-    }, [isfocused,editor,getPos,node,cell]);
-
+    }, [isFocused, editor, getPos, node, cell, parentTable]);
 
     return (
         <NodeViewWrapper
             className="react-component-with-content"
             onClick={() => {
-                setIsfocused(true);
+                setIsFocused(true);
             }}
             onMouseEnter={(event) => {
                 if (event.target.tagName === 'DIV') {
                     setCell(event.target);
                 }
+                setIsFocused(from >= nodeFrom && to <= nodeTo);
             }}
-            // onMouseLeave={() => { setIsfocused(false)}}
+            onMouseLeave={() => {
+                setIsFocused(from >= nodeFrom && to <= nodeTo);
+            }}
             style={{
                 display: "flex",
                 width: '100%',
@@ -72,7 +100,7 @@ const TableCellNodeView = ({editor, getPos, node }) => {
                 height: 'auto',
             }}>
             <NodeViewContent className="content" style={{ width: '100%', margin: '0', padding: '0' }} />
-            {isfocused && (
+            {isFocused && (
                 <button
                     className="label"
                     ref={dropdownButtonRef}
