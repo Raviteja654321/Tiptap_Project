33c59fe58e997d7e8f188c3dc7cd22b2d7d5f65e
raviteja.nagulapalli <raviteja.nagulapalli@sprinklr.com>
2024-06-24 03:06:56 +0530

    buttons using tippy.js

--- src/extensions/CustomTable.js
+++ src/extensions/CustomTable.js
@@ -1,6 +1,9 @@
 import Table from '@tiptap/extension-table';
-import { Plugin, PluginKey } from "@tiptap/pm/state"
-import { Decoration, DecorationSet } from "@tiptap/pm/view"
+import { Plugin, PluginKey } from "@tiptap/pm/state";
+import { Decoration, DecorationSet } from "@tiptap/pm/view";
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const CustomTable = Table.extend({
     addOptions() {
@@ -21,65 +24,53 @@ const CustomTable = Table.extend({
                 key: new PluginKey("addRowColumnButton"),
                 props: {
                     decorations: ({ doc, selection }) => {
-                        const decorations = []
+                        const decorations = [];
 
                         const addColumnLeft = () => {
                             this.editor.chain().focus().addColumnBefore().run();
-                        }
+                        };
 
                         const addRowAbove = () => {
-                            this.editor.chain().focus().addRowAfter().run() ;
-                        }
+                            this.editor.chain().focus().addRowAfter().run();
+                        };
 
                         doc.descendants((node, pos) => {
                             if (node.type.name === "table") {
-                                // display button to the left of each column on hovering between two columns
+                                // Display button to the left of each column on hovering between two columns
                                 node.firstChild.content.forEach((cell, index) => {
                                     const decoration = Decoration.widget(
                                         pos + 1,
                                         () => {
-                                            const button = document.createElement("button")
-                                            button.className = 'add-column-button'
-                                            button.innerHTML = '+'
+                                            const button = document.createElement("button");
+                                            button.className = 'add-column--button';
+                                            button.innerHTML = '<i class="fas fa-plus"></i>';
                                             button.addEventListener("click", event => {
-                                                event.preventDefault()
-                                                event.stopPropagation()
-                                                addColumnLeft()
-                                            })
-                                            return button
+                                                event.preventDefault();
+                                                event.stopPropagation();
+                                                addColumnLeft();
+                                            });
+
+                                            tippy(button, {
+                                                content: 'Add column',
+                                                placement: 'left',
+                                            });
+
+                                            return button;
                                         },
                                         { side: -1 }
-                                    )
-                                    decorations.push(decoration)
-                                })
+                                    );
+                                    decorations.push(decoration);
+                                });
 
-                                // display button to the top of each row on hovering between two rows
-                                node.content.forEach((row, rowIndex) => {
-                                    row.content.forEach((cell, cellIndex) => {
-                                        if (1) {
-                                            const decoration = Decoration.widget(
-                                                pos + row.pos + 1,
-                                                () => {
-                                                    const button = document.createElement("button")
-                                                    button.className = 'add-row-button'
-                                                    button.innerHTML = '+'
-                                                    button.onclick = addRowAbove;
-                                                    return button
-                                                },
-                                                { side: -1 }
-                                            )
-                                            decorations.push(decoration)
-                                        }
-                                    })
-                                })
+
                             }
-                        })
+                        });
 
-                        return DecorationSet.create(doc, decorations)
+                        return DecorationSet.create(doc, decorations);
                     }
                 }
             })
-        ]
+        ];
     }
 });
 
