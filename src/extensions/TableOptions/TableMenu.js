2bacddbfe3da679e89dba2a2deadc98209f5caf1
raviteja.nagulapalli <raviteja.nagulapalli@sprinklr.com>
2024-06-24 12:45:25 +0530

    Indentation settled

--- src/extensions/TableOptions/TableMenu.js
+++ src/extensions/TableOptions/TableMenu.js
@@ -4,55 +4,55 @@ import React, { useEffect, useState } from "react"
 import { useCurrentEditor } from "@tiptap/react"
 
 export const TableMenu = props => {
-  const [element, setElement] = useState(null)
-  const { editor: currentEditor } = useCurrentEditor()
+    const [element, setElement] = useState(null)
+    const { editor: currentEditor } = useCurrentEditor()
 
-  useEffect(() => {
-    if (!element) {
-      return
-    }
+    useEffect(() => {
+        if (!element) {
+            return
+        }
 
-    if (props.editor?.isDestroyed || currentEditor?.isDestroyed) {
-      return
-    }
+        if (props.editor?.isDestroyed || currentEditor?.isDestroyed) {
+            return
+        }
 
-    const {
-      pluginKey = "TableMenu",
-      editor,
-      tippyOptions = {},
-      updateDelay,
-      shouldShow = null
-    } = props
+        const {
+            pluginKey = "TableMenu",
+            editor,
+            tippyOptions = {},
+            updateDelay,
+            shouldShow = null
+        } = props
 
-    const menuEditor = editor || currentEditor
+        const menuEditor = editor || currentEditor
 
-    if (!menuEditor) {
-      console.warn(
-        "TableMenu component is not rendered inside of an editor component or does not have editor prop."
-      )
-      return
-    }
+        if (!menuEditor) {
+            console.warn(
+                "TableMenu component is not rendered inside of an editor component or does not have editor prop."
+            )
+            return
+        }
 
-    const plugin = TableMenuPlugin({
-      updateDelay,
-      editor: menuEditor,
-      element,
-      pluginKey,
-      shouldShow,
-      tippyOptions
-    })
+        const plugin = TableMenuPlugin({
+            updateDelay,
+            editor: menuEditor,
+            element,
+            pluginKey,
+            shouldShow,
+            tippyOptions
+        })
 
-    menuEditor.registerPlugin(plugin)
-    return () => menuEditor.unregisterPlugin(pluginKey)
-  }, [props, props.editor, currentEditor, element])
+        menuEditor.registerPlugin(plugin)
+        return () => menuEditor.unregisterPlugin(pluginKey)
+    }, [props, props.editor, currentEditor, element])
 
-  return (
-    <div
-      ref={setElement}
-      className={props.className}
-      style={{ visibility: "hidden" }}
-    >
-      {props.children}
-    </div>
-  )
+    return (
+        <div
+            ref={setElement}
+            className={props.className}
+            style={{ visibility: "hidden" }}
+        >
+            {props.children}
+        </div>
+    )
 }
--- src/extensions/TableOptions/table-menu.js
+++ src/extensions/TableOptions/table-menu.js
@@ -1,36 +1,36 @@
 import { Extension } from "@tiptap/core"
 
-import {TableMenuPlugin } from "./table-menu-plugin"
+import { TableMenuPlugin } from "./table-menu-plugin"
 
 const TableMenuExtension = Extension.create({
-  name: "tableMenu",
+    name: "tableMenu",
 
-  addOptions() {
-    return {
-      element: null,
-      tippyOptions: {},
-      pluginKey: "tableMenu",
-      updateDelay: undefined,
-      shouldShow: null
+    addOptions() {
+        return {
+            element: null,
+            tippyOptions: {},
+            pluginKey: "tableMenu",
+            updateDelay: undefined,
+            shouldShow: null
+        }
+    },
+
+    addProseMirrorPlugins() {
+        if (!this.options.element) {
+            return []
+        }
+
+        return [
+            TableMenuPlugin({
+                pluginKey: this.options.pluginKey,
+                editor: this.editor,
+                element: this.options.element,
+                tippyOptions: this.options.tippyOptions,
+                updateDelay: this.options.updateDelay,
+                shouldShow: this.options.shouldShow
+            })
+        ]
     }
-  },
-
-  addProseMirrorPlugins() {
-    if (!this.options.element) {
-      return []
-    }
-
-    return [
-      TableMenuPlugin({
-        pluginKey: this.options.pluginKey,
-        editor: this.editor,
-        element: this.options.element,
-        tippyOptions: this.options.tippyOptions,
-        updateDelay: this.options.updateDelay,
-        shouldShow: this.options.shouldShow
-      })
-    ]
-  }
 })
 
 export default TableMenuExtension;
